ac9a85f0a259e111c02802a352c96cba41672b46
Yashwanth Reddy Virupaksha <yashwanthvreddy@gmail.com>
2018-03-13 16:26:44 +0530

    added generator

---
+++ main.py
@@ -0,0 +1,97 @@
+"""An interface to train, test and inference the image captioning problem."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+
+from caption_generator import *
+from utils.data_util import generate_captions
+from utils.config import Config
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--mode",
+    type=str,
+    help="In what model would you like to use Train | Test | Eval?",
+    choices=[
+        "train",
+        "test",
+        "eval"],
+    required=True)
+parser.add_argument(
+    "--resume",
+    help="make model training resumable",
+    action="store_true")
+parser.add_argument(
+    "--caption_path",
+    type=str,
+    help="The location of Flicker8k Captions \'.token\' file.")
+parser.add_argument(
+    "--feature_path",
+    type=str,
+    help="The location of Flickr8k image features: features.npy file.")
+parser.add_argument(
+    "--inception_path",
+    type=str,
+    help="The location of \'inception_v4.pb\' to encode images.",
+    default="Conv-Network/inception_v4.pb")
+parser.add_argument(
+    "--saveencoder",
+    help="Save Encoder graph in model/Encoder/",
+    action="store_true")
+parser.add_argument(
+    "--savedecoder",
+    help="Save Decoder graph in model/Decoder/",
+    action="store_true")
+parser.add_argument(
+    "--image_path",
+    type=str,
+    help="Image path required during test mode for Generation of Captions.")
+parser.add_argument(
+    "--load_image",
+    help="If mode is test then, displays and stores image with generated caption",
+    action="store_true")
+parser.add_argument(
+    "--validation_data",
+    type=str,
+    help="If mode is eval then, Path to the Validation Data for evaluation")
+FLAGS = parser.parse_args()
+config = Config(vars(FLAGS))
+
+if(config.mode == "train"):
+    data = generate_captions(
+        config, FLAGS.caption_path, FLAGS.feature_path)
+    model = Caption_Generator(config, data=data)
+    loss, inp_dict = model.build_train_graph()
+    model.train(loss, inp_dict)
+
+elif config.mode == "test":
+    if os.path.exists(image_path):
+        model = Caption_Generator(config)
+        model.decode(FLAGS.image_path)
+    else:
+        print("Please provide a valid image path.\n Usage:\n python main.py --mode test --image_path VALID_PATH")
+
+elif config.mode == "eval":
+    config.mode = "test"
+    config.batch_decode = True
+    print(FLAGS.validation_data)
+    if os.path.exists(FLAGS.validation_data):
+        features = np.load(FLAGS.validation_data)
+        #with open("Dataset/Validation_Captions.txt") as f:
+        #    data = f.readlines()
+        with open("Dataset/image_info_test2014.json",'r') as f:
+            data=json.load(f)
+
+        #filenames = [caps.split('\t')[0].split('#')[0] for caps in data]
+        filenames  = sorted([d["file_name"].split('.')[0] for d in data['images']])
+        #captions = [caps.replace('\n', '').split('\t')[1] for caps in data]
+        #features, captions = validation_data[:, 0], validation_data[:, 1]
+        features = np.array([feat.astype(float) for feat in features])
+        model = Caption_Generator(config)
+        generated_captions = model.batch_decoder(filenames, features)
---
+++ utils/data_util.py
@@ -0,0 +1,150 @@
+import numpy as np
+import pandas as pd
+from collections import Counter
+from nltk.tokenize import word_tokenize
+import pickle
+import json
+import os
+
+
+max_len = 20
+word_threshold = 2
+counter = None
+
+
+def load_features(feature_path):
+    features = np.load(feature_path)
+    features = np.repeat(features, 5, axis=0)
+    features = np.array([feat.astype(float) for feat in features])
+    print("Features Loaded", feature_path)
+    return features
+
+
+def get_data(required_files):
+    ret = []
+    for fil in required_files:
+        ret.append(np.load("Dataset/" + fil + ".npy"))
+
+    training_data = ret[2]
+    features, captions = training_data[:, 0], training_data[:, 1]
+    struct_features = np.zeros((len(features), 1536))
+    for idx, row in enumerate(features):
+        struct_features[idx] = row
+
+    ret[0] = ret[0].tolist()
+    ret[1] = ret[1].tolist()
+    ret[2] = struct_features
+    ret.append(captions)
+    return ret
+
+
+def preprocess_flickr_captions(filenames, captions):
+    global max_len
+    print("Preprocessing Captions...")
+    cap_token_df = pd.DataFrame()
+    cap_token_df['FileNames'] = filenames
+    cap_token_df['caption'] = captions
+    cap_token_df['caption'] = cap_token_df['caption'].apply(word_tokenize).apply(
+        lambda x: x[:max_len]).apply(" ".join).str.lower()
+    return cap_token_df
+
+def split_dataset(df, features, ratio=0.8):
+    split_idx = int(df.shape[0] * ratio)
+    print("Data Statistics:")
+    print("# Records Total Data: ", df.shape[0])
+    print("# Records Training Data: ", split_idx)
+    print("# Records Validation Data: ", df.shape[0] - split_idx)
+    print("Ration of Training: Validation = ", ratio * 100, ":", 100 - (ratio * 100))
+    val_features = features[split_idx:]
+    val_captions = np.array(df.caption)[split_idx:]
+    np.save("Dataset/valid_data", zip(val_features, val_captions))
+    return df[:split_idx], features[:split_idx]
+
+
+def generate_vocab(capt_token_df):
+    global max_len, word_threshold, counter
+    print("Generating Vocabulary")
+
+    vocab = dict([w for w in counter.items() if w[1] >= word_threshold])
+    vocab["<UNK>"] = len(counter) - len(vocab)
+    vocab["<PAD>"] = capt_token_df.caption.str.count("<PAD>").sum()
+    vocab["<S>"] = capt_token_df.caption.str.count("<S>").sum()
+    vocab["</S>"] = capt_token_df.caption.str.count("</S>").sum()
+
+    wtoidx = {}
+    wtoidx["<S>"] = 1
+    wtoidx["</S>"] = 2
+    wtoidx["<PAD>"] = 0
+    wtoidx["<UNK>"] = 3
+    print("Generating Word to Index and Index to Word")
+    i = 4
+    for word in vocab.keys():
+        if word not in ["<S>", "</S>", "<PAD>", "<UNK>"]:
+            wtoidx[word] = i
+            i += 1
+    print("Size of Vocabulary", len(vocab))
+    return vocab, wtoidx
+
+
+def pad_captions(capt_token_df):
+    global max_len
+    print("Padding Caption <PAD> to Max Length", max_len, "+ 2 for <S> and </S>")
+    capt_dfPadded = capt_token_df.copy()
+    capt_dfPadded['caption'] = "<S> " + capt_dfPadded['caption'] + " </S>"
+    max_len = max_len + 2
+    for i, row in capt_dfPadded.iterrows():
+        capt = row['caption']
+        capt_len = len(capt.split())
+        if(capt_len < max_len):
+            pad_len = max_len - capt_len
+            pad_buf = "<PAD> " * pad_len
+            pad_buf = pad_buf.strip()
+            capt_dfPadded.set_value(i, 'caption', capt + " " + pad_buf)
+    return capt_dfPadded
+
+
+def generate_captions(config,
+    capt_path='Dataset/Flickr8k_text/Flickr8k.token.txt',
+    feat_path='Dataset/features.npy'):
+
+    required_files = ["vocab", "wordmap", "train_data"]
+    generate = False
+    for f in required_files:
+        if not os.path.isfile('Dataset/' + f + ".npy"):
+            generate = True
+            print("Required Files not present. Regenerating Data.")
+            break
+    if not generate:
+        print("Dataset Present; Skipping Generation.")
+        return get_data(required_files)
+
+    global max_len, word_threshold, counter
+    max_len = config.max_len
+    word_threshold = config.word_threshold
+
+    print("Loading Caption Data", capt_path)
+    with open(capt_path, 'r') as f:
+        data = f.readlines()
+    filenames = [capts.split('\t')[0].split('#')[0] for capts in data]
+    captions = [capts.replace('\n', '').split('\t')[1] for capts in data]
+    capt_token_df = preprocess_flickr_captions(filenames, captions)
+
+    features = load_features(feat_path)
+    print(features.shape, capt_token_df.shape)
+    index = np.random.permutation(features.shape[0])
+    capt_token_df = capt_token_df.iloc[index]
+    features = features[index]
+    capt_token_df, features = split_dataset(capt_token_df, features)
+    counter = Counter()
+    for i, row in capt_token_df.iterrows():
+        counter.update(row["caption"].lower().split())
+    capt_token_df = pad_captions(capt_token_df)
+    vocab, wtoidx = generate_vocab(capt_token_df)
+    captions = np.array(capt_token_df.caption)
+
+    np.save("Dataset/train_data", list(zip(features, captions)))
+    np.save("Dataset/wordmap", wtoidx)
+    np.save("Dataset/vocab", vocab)
+
+    print("Preprocessing Complete")
+    return get_data(required_files)
